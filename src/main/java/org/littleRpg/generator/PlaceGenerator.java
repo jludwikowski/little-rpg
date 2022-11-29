3a9fea1f199643ed5cea5628376536f4e374bd3d
Piotr <piotrblonski3@gmail.com>
2022-11-29 18:43:19 +0100

    betterBiomeGeneraton

--- src/main/java/org/littleRpg/generator/PlaceGenerator.java
+++ src/main/java/org/littleRpg/generator/PlaceGenerator.java
@@ -52,63 +52,37 @@ public class PlaceGenerator extends Generator<MapPlace> {
         Biome newBiome = null;
         Biome lastBiomeX;
         Biome lastBiomeY;
+        Biome lastBiome;
 
        if (placeX == null && placeY == null){
             newBiome = Biome.values()[Roller.pickNumberFrom(Biome.values().length)];
         }
        else {
-           if(placeX != null) {
-               lastBiomeX = placeX.biome;
-               int index1 = Arrays.binarySearch(Biome.values(), lastBiomeX);
+           lastBiome = placeX != null ? placeX.biome : null;
+           lastBiome = placeY != null && lastBiome == null ? placeY.biome : lastBiome;
+           int index1 = Arrays.binarySearch(Biome.values(), lastBiome);
+           double roll = Math.random()*100;
 
-               double roll = Math.random()*100;
-
-               if (roll <50) {
-                   newBiome = lastBiomeX;
-               }
-
-               if (roll > 50 && roll < 75 && index1 != Biome.values().length - 1){
-                   newBiome = Biome.values()[index1 + 1];
-               }
-               if(roll >= 75 && index1 != 0) {
-                   newBiome = Biome.values()[index1 - 1];
-               }
-               if(roll > 50 && roll < 75 && index1 == Biome.values().length - 1) {
-                   newBiome = Biome.values()[0];
-               }
-               if(roll >= 75 && index1 == 0) {
-                   newBiome = Biome.values()[Biome.values().length-1];
-               }
-
+           if (roll <50) {
+               newBiome = lastBiome;
            }
-           if(placeY != null){
-               lastBiomeY = placeY.biome;
-               int index2 = Arrays.binarySearch(Biome.values(), lastBiomeY);
-               double roll = Math.random()*100;
-
-               if (roll <50) {
-                   newBiome = lastBiomeY;
-               }
-
-               if (roll > 50 && roll < 75 && index2 != Biome.values().length - 1){
-                   newBiome = Biome.values()[index2 + 1];
-               }
-               if(roll >= 75 && index2 != 0) {
-                   newBiome = Biome.values()[index2 - 1];
-               }
-               if(roll > 50 && roll < 75 && index2 == Biome.values().length - 1) {
-                   newBiome = Biome.values()[0];
-               }
-               if(roll >= 75 && index2 == 0) {
-                   newBiome = Biome.values()[Biome.values().length-1];
-               }
-
+           if (roll > 50 && roll < 75 && index1 != Biome.values().length - 1){
+               newBiome = Biome.values()[index1 + 1];
+           }
+           if(roll >= 75 && index1 != 0) {
+               newBiome = Biome.values()[index1 - 1];
+           }
+           if(roll > 50 && roll < 75 && index1 == Biome.values().length - 1) {
+               newBiome = Biome.values()[0];
+           }
+           if(roll >= 75 && index1 == 0) {
+               newBiome = Biome.values()[Biome.values().length-1];
            }
 
            if(placeX != null && placeY != null) {
-               double roll = Math.random() * 100;
+               roll = Math.random() * 100;
                lastBiomeX = placeX.biome;
-               int index1 = Arrays.binarySearch(Biome.values(), lastBiomeX);
+               index1 = Arrays.binarySearch(Biome.values(), lastBiomeX);
                lastBiomeY = placeY.biome;
                int index2 = Arrays.binarySearch(Biome.values(), lastBiomeY);
 
--- src/main/java/org/littleRpg/generator/WorldGenerator.java
+++ src/main/java/org/littleRpg/generator/WorldGenerator.java
@@ -6,21 +6,23 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class WorldGenerator {
-    MapPlace lastPlace =  null;
     PlaceGenerator placeGenerator = new PlaceGenerator();
     MapPlace[][][] world = new MapPlace[MAX_Z][MAX_Y][MAX_X];
     static int MAX_Z = 1;
     static int MAX_Y = 20;
     static int MAX_X = 20;
     public MapPlace[][][] generateWorld(){
+        MapPlace place =  null;
+        MapPlace placeX;
+        MapPlace placeY;
         for(int i=0;i<MAX_Z;i++) {
             for(int j=0;j<MAX_Y;j++) {
                 for(int k=0;k<MAX_X;k++) {
-                    if(j != 0 &&)
-                    MapPlace place = placeGenerator.getEntity(this.world[i][j-1][k], this.world[i][j][k-1] );
-                    lastPlace = place;
-                    this.world[i][j][k] = place;
+                    placeX = j>0 ? this.world[i][j-1][k] : null;
+                    placeY = k>0 ? this.world[i][j][k-1] : null;
+                    place = placeGenerator.getEntity(placeX, placeY);
 
+                    this.world[i][j][k] = place;
                 }
             }
         }
